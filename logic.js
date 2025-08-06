e7a7942b85a40afabe47635f50c9e629fb90a16b
codeewithhmee <samirbhandari1231@gmail.com>
2025-08-06 23:04:07 +0545

    Update logic.js

--- logic.js
+++ logic.js
@@ -10,14 +10,32 @@ function togg(e){
     }
     
 }
+let d=true;
+
 set();
 function set(){
+
+    let fixedName1=document.getElementById('name').value;
+    let image;
+    if(fixedName1=="salfet sigdel"){
+        image="https://i.ibb.co/svDnSDJY/salfet.png";
+    }else if(fixedName1=="samir bhandari"){
+        image="https://i.ibb.co/j2TnmrF/samir.png";
+    }
+    else{
+        image=document.getElementById('imgonline').value;
+    }
+    if(d){
+        image="https://static.vecteezy.com/system/resources/thumbnails/005/346/410/small_2x/close-up-portrait-of-smiling-handsome-young-caucasian-man-face-looking-at-camera-on-isolated-light-gray-studio-background-photo.jpg";
+        d=false;
+    }
+    console.log(fixedName1.toLowerCase());
     document.getElementById('name_').innerText="Name: "+document.getElementById('name').value;
     document.getElementById('address_').innerText="Address: "+document.getElementById('address').value;
     document.getElementById('dob_').innerText="DOB: "+document.getElementById('dob').value;
     document.getElementById('fhname_').innerText="F/H Name: "+document.getElementById('fhname').value;
     document.getElementById('name_').innerText="Name: "+document.getElementById('name').value;
-    document.getElementById('pp').src=document.getElementById('imgonline').value;
+    document.getElementById('pp').src=image;
     document.getElementById('doi_').innerText="D.O.I.:"+document.getElementById('doi').value;
     document.getElementById('doe_').innerText="D.O.E.:"+document.getElementById('doe').value;
     document.getElementById('pn_').innerText="Passport:"+document.getElementById('pn').value;
